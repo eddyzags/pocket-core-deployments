427fdb59837ef551d49949ec67fbe9f7c46183fa
eddyzags <zagabe.ed@gmail.com>
2021-02-24 16:40:15 +0100

    add stop signal handler + trap SIGTERM signal

--- docker/command.sh
+++ docker/command.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/expect
 
+proc stopsignal_handler () {
+    if [ $pid -ne 0 ]; then
+      kill -SIGTERM "$pid"
+      wait "$pid"
+    fi
+    exit 0; # 128 + 15 -- SIGTERM
+}
+
 set command $argv; # Grab the first command line parameter
 set timeout -1
 
@@ -7,7 +15,7 @@ spawn sh -c "cp /tmp/*.json /home/app/.pocket/config/"
 sleep 2
 if { $env(POCKET_CORE_KEY) eq "" }  {
     log_user 0
-    spawn sh -c "$command"
+    spawn sh -c "$command &"
     send -- "$env(POCKET_CORE_PASSPHRASE)\n"
     log_user 1
 
@@ -22,8 +30,19 @@ if { $env(POCKET_CORE_KEY) eq "" }  {
     send -- "$env(POCKET_CORE_PASSPHRASE)\n"
     expect eof
     log_user 1
-    spawn sh -c "$command"
+    spawn sh -c "$command &"
 }
+
+# Kills the last background process, which is `tail -f /dev/null` and execute the term handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
 
 expect eof
 
